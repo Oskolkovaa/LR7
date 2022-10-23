820717459b79f4c98789d71a5e866d7aa71e2da8
yaolg <yaolg@DESKTOP-PS4EPC5>
2022-10-23 15:35:42 +0300

    Добавьте файлы проекта.

---
+++ LR7/UserStorage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR7
+{
+    internal class UserStorage
+    {
+        private Dictionary<AuthorizeInfo, UserInfo> data;
+
+        public UserStorage()
+        {
+            data= new Dictionary<AuthorizeInfo, UserInfo>();
+            data.Add(new AuthorizeInfo("Lera", "1"), new UserInfo("Oskokova V.E.", "oskolkova@mail.ru","Stavropol"));
+            data.Add(new AuthorizeInfo("Vova", "2"), new UserInfo("Nekrut V.S.", "NekrutVS@mail.ru", "Stavropol"));
+            data.Add(new AuthorizeInfo("Adam", "3"), new UserInfo("Zaigov A.D.", "ZaigovAD@yandex.ru", "Moscow"));
+        }
+
+        public UserInfo GetUser(AuthorizeInfo ob)
+        {
+            if(data.ContainsKey(ob)) return data[ob];
+            else return null;
+        }
+    }
+}
